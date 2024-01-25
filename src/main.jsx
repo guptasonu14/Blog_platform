1a6e508825eeef3767fca9301b33368ff251e186
guptasonu14 <sguptabudha@gmail.com>
2024-01-25 10:27:30 +0530

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
 
@@ -79,10 +79,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Routes>{router}</Routes>
-      </Router>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
-
